1561819a62f50c583c11234c16ab9b10a4a365e3
astepanov <rdliteling@gmail.com>
2022-09-30 19:44:59 +0300

    Push test

--- Project/Assets/_Tech/Scripts/Logic/BattleSystem/BattleHandler.cs
+++ Project/Assets/_Tech/Scripts/Logic/BattleSystem/BattleHandler.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.Rendering.Universal;
 
 public class BattleHandler {
